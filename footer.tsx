28485c72efb7a153515515c8f86cb2a1c749b830
mtsmith21 <mtsmith21@gmail.com>
2025-12-04 14:52:51 +0100

    Create footer.tsx

---
+++ footer.tsx
@@ -0,0 +1,16 @@
+import { AppLogo } from "@/components/icons";
+
+export function Footer() {
+  return (
+    <footer className="border-t">
+      <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
+        <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
+          <AppLogo className="h-6 w-6 text-primary" />
+          <p className="text-center text-sm leading-loose md:text-left">
+            &copy; {new Date().getFullYear()} Rate My Operator. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
